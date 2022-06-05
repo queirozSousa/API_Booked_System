1618e988068782f80005d9bdc154c7758b4c8479
Queiroz Sousa <queirozmacamo82@gmail.com>
2022-06-05 12:10:52 +0200

    Feat:Migration of Attendence, Counter and Deposit

---
+++ database/migrations/2022_06_05_095521_create_attendece_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendece', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignID('user_id')
+            ->constrained()
+            ->onDelete("CASCADE")
+            ->onUpdate('CASCADE');
+            $table->dateTime('date');
+            $table->string('adress');
+            $table->string('name_counter');
+            $table->string('b_i');
+            $table->string('assunto');
+            $table->string('nuit');
+            $table->string('numero_telefone');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendece');
+    }
+};
---
+++ database/migrations/2022_06_05_100150_create_deposit_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deposit', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignID('user_id')
+            ->constrained()
+            ->onDelete("CASCADE")
+            ->onUpdate('CASCADE');
+            $table->dateTime('date');
+            $table->string('adress');
+            $table->string('name_counter');
+            $table->string('b_i');
+            $table->string('name_of_recipient');
+            $table->string('number_of_recipient');
+            $table->string('amout');
+            $table->string('nuit');
+            $table->string('number_cell');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deposit');
+    }
+};
